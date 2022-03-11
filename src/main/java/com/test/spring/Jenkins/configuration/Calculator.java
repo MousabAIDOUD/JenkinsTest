8c255e20ab797fde4fc96d6c87f43b87cc182a74
MAIDOUD <mousab.aidoud@sii.fr>
2022-03-11 11:54:55 +0100

    Adding class and tests

---
+++ src/main/java/com/test/spring/Jenkins/configuration/Calculator.java
@@ -0,0 +1,20 @@
+package com.test.spring.Jenkins.configuration;
+
+public class Calculator {
+
+    public static int sum(int a, int b ){
+        return  a+b;
+    }
+    public static int multiple(int a, int b ){
+        return  a*b;
+    }
+    public static float divison(int a, int b ){
+        if (b == 0){
+            return 0;
+        }
+        return a/b;
+    }
+    public static float sous(int a, int b ){
+        return  a-b;
+    }
+}
---
+++ src/test/java/com/test/spring/Jenkins/configuration/CalculatorTest.java
@@ -0,0 +1,32 @@
+package com.test.spring.Jenkins.configuration;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CalculatorTest {
+
+    @Test
+    void sum() {
+        //GIVEN 1 + 2
+        assertEquals(4 , Calculator.sum(1,3) );
+    }
+
+    @Test
+    void multiple() {
+        //GIVEN 9*9
+        assertEquals(81 , Calculator.multiple(9,9) );
+    }
+
+    @Test
+    void divison() {
+        //GIVEN 8/4
+        assertEquals(4 , Calculator.divison(8,2) );
+    }
+
+    @Test
+    void sous() {
+        //GIVEN 10-5
+        assertEquals(5 , Calculator.sous(10,5) );
+    }
+}
